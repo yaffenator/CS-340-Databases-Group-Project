67c692e03f2e4d5a213cbf9960d4d599702cb3a9
yaffenator <noamyaffe19@gmail.com>
2025-11-12 10:12:53 -0800

    Updated frontend & queries to meet requirements

--- app.py
+++ app.py
@@ -14,15 +14,52 @@ app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 mysql = MySQL(app)
 
 @app.route("/")
-def main_movies_page():
-    query = 'SELECT * FROM Movies;'
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-    return render_template('index.html', movies = results)
+def landing_page():
+    return render_template('index.html')
+
+@app.route("/movies")
+def movies_page():
+    # Movies query
+    query1 = 'SELECT * FROM Movies;'
+    cur1 = mysql.connection.cursor()
+    cur1.execute(query1)
+    results1 = cur1.fetchall()
+
+    # Genres category JOIN query
+    query2 = 'SELECT category FROM Genres JOIN Movies on Genres.idGenre = Movies.idGenre GROUP BY Movies.idGenre;'
+    cur2 = mysql.connection.cursor()
+    cur2.execute(query2)
+    results2 = cur2.fetchall()
+
+    # Directors firstName & lastName JOIN query
+    query3 = 'SELECT Directors.firstName, Directors.lastName FROM Directors JOIN Movies_has_Directors ON Directors.idDirector = Movies_has_Directors.idDirector JOIN Movies ON Movies_has_Directors.idMovie = Movies.idMovie;'
+    cur3 = mysql.connection.cursor()
+    cur3.execute(query3)
+    results3 = cur3.fetchall()
+
+    # Movies_has_Directors query (for backend id association)
+    query4 = 'SELECT * FROM Movies_has_Directors;'
+    cur4 = mysql.connection.cursor()
+    cur4.execute(query4)
+    results4 = cur4.fetchall()
+
+    # Actors firstName & lastName JOIN query
+    query5 = 'SELECT Actors.firstName, Actors.lastName FROM Actors JOIN Movies_has_Actors ON Actors.idActor = Movies_has_Actors.idActor JOIN Movies ON Movies_has_Actors.idMovie = Movies.idMovie;'
+    cur5 = mysql.connection.cursor()
+    cur5.execute(query5)
+    results5 = cur5.fetchall()
+
+    # Movies_has_Actors query (for backend id association)
+    query6 = 'SELECT * FROM Movies_has_Actors;'
+    cur6 = mysql.connection.cursor()
+    cur6.execute(query6)
+    results6 = cur6.fetchall()
+
+    return render_template('movies.html', movies = results1, categories = results2, directors = results3, directors_intersection_data = results4, actors = results5, actors_intersection_data = results6)
 
 @app.route("/genres")
 def genres_page():
+    # Genres query
     query = 'SELECT * FROM Genres;'
     cur = mysql.connection.cursor()
     cur.execute(query)
@@ -31,35 +68,78 @@ def genres_page():
 
 @app.route("/directors")
 def directors_page():
-    query = 'SELECT * FROM Directors;'
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-    return render_template('directors.html', directors = results)
+    # Directors query
+    query1 = 'SELECT * FROM Directors;'
+    cur1 = mysql.connection.cursor()
+    cur1.execute(query1)
+    results1 = cur1.fetchall()
+
+    # Movies title JOIN query
+    query2 = 'SELECT Movies.idMovie, Movies.title FROM Movies JOIN Movies_has_Directors ON Movies.idMovie = Movies_has_Directors.idMovie JOIN Directors ON Movies_has_Directors.idDirector = Directors.idDirector GROUP BY Movies.idMovie'
+    cur2 = mysql.connection.cursor()
+    cur2.execute(query2)
+    results2 = cur2.fetchall()
+
+    # Movies_has_Directors query (for backend id association)
+    query3 = 'SELECT * FROM Movies_has_Directors'
+    cur3 = mysql.connection.cursor()
+    cur3.execute(query3)
+    results3 = cur3.fetchall()
+
+    return render_template('directors.html', directors = results1, movie_titles = results2, intersection_data = results3)
 
 @app.route("/actors")
 def actors_page():
-    query = 'SELECT * FROM Actors;'
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-    return render_template('actors.html', actors = results) 
+    # Actors query
+    query1 = 'SELECT * FROM Actors;'
+    cur1 = mysql.connection.cursor()
+    cur1.execute(query1)
+    results1 = cur1.fetchall()
 
-@app.route("/audiences")
+    # Movies title JOIN query
+    query2 = 'SELECT Movies.idMovie, Movies.title FROM Movies JOIN Movies_has_Actors ON Movies.idMovie = Movies_has_Actors.idMovie JOIN Actors ON Movies_has_Actors.idActor = Actors.idActor GROUP BY Movies.idMovie'
+    cur2 = mysql.connection.cursor()
+    cur2.execute(query2)
+    results2 = cur2.fetchall()
+
+    # Movies_has_Actors query (for backend id association)
+    query3 = 'SELECT * FROM Movies_has_Actors'
+    cur3 = mysql.connection.cursor()
+    cur3.execute(query3)
+    results3 = cur3.fetchall()
+
+    return render_template('actors.html', actors = results1, movie_titles = results2, intersection_data = results3) 
+
+@app.route("/audience")
 def audiences_page():
-    query = 'SELECT * FROM Audiences;'
+    # Audiences query
+    query1 = 'SELECT * FROM Audiences;'
     cur = mysql.connection.cursor()
-    cur.execute(query)
+    cur.execute(query1)
     results = cur.fetchall()
-    return render_template('audiences.html', audiences = results) 
+    return render_template('audience.html', audiences = results) 
 
 @app.route("/audience_reviews")
 def audience_reviews_page():
-    query = 'SELECT * FROM AudienceReviews;'
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-    return render_template('audience_reviews.html', audience_reviews = results) 
+    # Audience Reviews query
+    query1 = 'SELECT * FROM AudienceReviews;'
+    cur1 = mysql.connection.cursor()
+    cur1.execute(query1)
+    results1 = cur1.fetchall()
+
+    # Movies title JOIN query
+    query2 = 'SELECT title FROM Movies JOIN AudienceReviews on Movies.idMovie = AudienceReviews.idMovie GROUP BY Movies.idMovie;'
+    cur2 = mysql.connection.cursor()
+    cur2.execute(query2)
+    results2 = cur2.fetchall()
+
+    # Audiences firstName & lastName JOIN query
+    query3 = 'SELECT firstName, lastName FROM Audiences JOIN AudienceReviews on Audiences.idAudience = AudienceReviews.idAudience GROUP BY Audiences.idAudience;'
+    cur3 = mysql.connection.cursor()
+    cur3.execute(query3)
+    results3 = cur3.fetchall()
+
+    return render_template('audience_reviews.html', audience_reviews = results1, movie_titles = results2, audience_names = results3) 
 
 @app.route("/movies_has_directors")
 def movies_has_directors_page():
@@ -78,4 +158,4 @@ def movies_has_actors_page():
     return render_template('movies_has_actors.html', results = results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port = 5164, debug=True)
--- populate_db.sql
+++ populate_db.sql
@@ -4,291 +4,377 @@
 -- Date: October 30, 2025
 -- -----------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
+
 SET AUTOCOMMIT = 0;
 
 -- -----------------------------------------------------
 -- Genres Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Genres`;
-CREATE TABLE Genres(
-  idGenre INT AUTO_INCREMENT NOT NULL UNIQUE,
-  category VARCHAR(30) NOT NULL,
-  PRIMARY KEY (idGenre)
+
+CREATE TABLE Genres (
+    idGenre INT AUTO_INCREMENT NOT NULL UNIQUE,
+    category VARCHAR(30) NOT NULL,
+    PRIMARY KEY (idGenre)
 );
 
-INSERT INTO Genres(
-  category
-)
-VALUES(
-  "Action"
-),
-(
-  "Sci-Fi"
-),
-(
-  "Comedy"
-);
+INSERT INTO
+    Genres (category)
+VALUES ("Action"),
+    ("Sci-Fi"),
+    ("Family");
 
 -- -----------------------------------------------------
 -- Genres Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Movies`;
-CREATE TABLE Movies(
-  idMovie INT AUTO_INCREMENT NOT NULL UNIQUE,
-  title VARCHAR(145)NOT NULL,
-  releaseYear YEAR NOT NULL,
-  idGenre INT NOT NULL,
-  description VARCHAR(300) NOT NULL,
-  averageRating DECIMAL(3, 1) NOT NULL,
-  PRIMARY KEY (idMovie),
-  FOREIGN KEY (idGenre) REFERENCES Genres(idGenre)
-    ON DELETE CASCADE
+
+CREATE TABLE Movies (
+    idMovie INT AUTO_INCREMENT NOT NULL UNIQUE,
+    title VARCHAR(145) NOT NULL,
+    releaseYear YEAR NOT NULL,
+    idGenre INT NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    averageRating DECIMAL(3, 1) NOT NULL,
+    PRIMARY KEY (idMovie),
+    FOREIGN KEY (idGenre) REFERENCES Genres (idGenre) ON DELETE CASCADE
 );
 
-INSERT INTO Movies(
-  title,
-  releaseYear,
-  idGenre,
-  description,
-  averageRating
-)
-VALUES(
-  "Spider-Man: Across the Spider Verse",
-  2023,
-  (SELECT idGenre FROM Genres WHERE category = "Sci-Fi"),
-  "Spider-Man animated movies about Miles Morales",
-  9.5
-),
-(
-  "Avengers: Endgame",
-  2019,
-  (SELECT idGenre FROM Genres WHERE category = "Action"),
-  "End of the Avengers seiries",
-  8.7
-),
-(
-  "Home Alone",
-  1990,
-  (SELECT idGenre FROM Genres WHERE category = "Comedy"),
-  "A movie about a boy left at home alone",
-  6.8
-);
+INSERT INTO
+    Movies (
+        title,
+        releaseYear,
+        idGenre,
+        description,
+        averageRating
+    )
+VALUES (
+        "Spider-Man: Across the Spider Verse",
+        2023,
+        (
+            SELECT idGenre
+            FROM Genres
+            WHERE
+                category = "Sci-Fi"
+        ),
+        "Spider-Man animated movies about Miles Morales",
+        9.5
+    ),
+    (
+        "Avengers: Endgame",
+        2019,
+        (
+            SELECT idGenre
+            FROM Genres
+            WHERE
+                category = "Action"
+        ),
+        "End of the Avengers seiries",
+        8.7
+    ),
+    (
+        "UP",
+        2009,
+        (
+            SELECT idGenre
+            FROM Genres
+            WHERE
+                category = "Family"
+        ),
+        "A movie about a boy who met an old man and flew in his house with balloons",
+        8.8
+    );
 
 -- -----------------------------------------------------
 -- Directors Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Directors`;
-CREATE TABLE Directors(
-  idDirector INT AUTO_INCREMENT NOT NULL UNIQUE,
-  firstName VARCHAR(45) NOT NULL,
-  lastName VARCHAR(45) NOT NULL,
-  middleName VARCHAR(45),
-  PRIMARY KEY (idDirector)
+
+CREATE TABLE Directors (
+    idDirector INT AUTO_INCREMENT NOT NULL UNIQUE,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    middleName VARCHAR(45),
+    PRIMARY KEY (idDirector)
 );
 
-INSERT INTO Directors(
-  firstName,
-  lastName,
-  middleName
-)
-VALUES(
-  "Joaquim",
-  "Dos Santos",
-  NULL
-),
-(
-  "Kemp",
-  "Powers",
-  NULL
-),
-(
-  "Justin",
-  "Thompson",
-  "Kent"
-);
+INSERT INTO
+    Directors (
+        firstName,
+        lastName,
+        middleName
+    )
+VALUES ("Joaquim", "Dos Santos", NULL),
+    ("Kemp", "Powers", NULL),
+    ("Justin", "Thompson", "Kent");
 
 -- -----------------------------------------------------
 -- Actors Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Actors`;
-CREATE TABLE Actors(
-  idActor INT AUTO_INCREMENT NOT NULL UNIQUE,
-  firstName VARCHAR(45) NOT NULL,
-  lastName VARCHAR(45) NOT NULL,
-  middleName VARCHAR(45),
-  PRIMARY KEY (idActor)
+
+CREATE TABLE Actors (
+    idActor INT AUTO_INCREMENT NOT NULL UNIQUE,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    middleName VARCHAR(45),
+    PRIMARY KEY (idActor)
 );
 
-INSERT INTO Actors(
-  firstName,
-  lastName,
-  middleName
-)
-VALUES(
-  "Shameik",
-  "Moore",
-  NULL
-),
-(
-  "Hailee",
-  "Steinfeld",
-  NULL
-),
-(
-  "Brian",
-  "Henry",
-  "Tyree"
-);
+INSERT INTO
+    Actors (
+        firstName,
+        lastName,
+        middleName
+    )
+VALUES ("Shameik", "Moore", NULL),
+    ("Hailee", "Steinfeld", NULL),
+    ("Brian", "Henry", "Tyree");
 
 -- -----------------------------------------------------
 -- Audiences Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Audiences`;
-CREATE TABLE Audiences(
-  idAudience INT AUTO_INCREMENT NOT NULL UNIQUE,
-  firstName VARCHAR(45) NOT NULL,
-  lastName VARCHAR(45) NOT NULL,
-  middleName VARCHAR(45),
-  email varchar(145) NOT NULL,
-  PRIMARY KEY (idAudience)
+
+CREATE TABLE Audiences (
+    idAudience INT AUTO_INCREMENT NOT NULL UNIQUE,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    middleName VARCHAR(45),
+    email varchar(145) NOT NULL,
+    PRIMARY KEY (idAudience)
 );
 
-INSERT INTO Audiences(
-  firstName,
-  lastName,
-  middleName,
-  email
-)
-VALUES(
-  "John",
-  "Adams",
-  NULL,
-  "Adamsj@OSU.edu"
-),
-(
-  "Adam",
-  "James",
-  "Curry",
-  "Jamesadam@OSU.edu"
-),
-(
-  "Jawan",
-  "Curry",
-  NULL,
-  "Curryjaw@OSU.edu"
-);
+INSERT INTO
+    Audiences (
+        firstName,
+        lastName,
+        middleName,
+        email
+    )
+VALUES (
+        "John",
+        "Adams",
+        NULL,
+        "Adamsj@OSU.edu"
+    ),
+    (
+        "Adam",
+        "James",
+        "Curry",
+        "Jamesadam@OSU.edu"
+    ),
+    (
+        "Jawan",
+        "Curry",
+        NULL,
+        "Curryjaw@OSU.edu"
+    );
 
 -- -----------------------------------------------------
 -- AudienceReviews Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `AudienceReviews`;
-CREATE TABLE AudienceReviews(
-  idAudienceReview INT AUTO_INCREMENT NOT NULL UNIQUE,
-  idMovie INT NOT NULL,
-  idAudience INT NOT NULL,
-  review VARCHAR(300) NOT NULL,
-  stars TINYINT NOT NULL
-    CHECK (stars BETWEEN 1 AND 5),
-  PRIMARY KEY (idAudienceReview),
-  FOREIGN KEY (idMovie) REFERENCES Movies(idMovie)
-    ON DELETE CASCADE,
-  FOREIGN KEY (idAudience) REFERENCES Audiences(idAudience)
-    ON DELETE CASCADE,
-  CONSTRAINT movie_audience UNIQUE(idMovie, idAudience)
+
+CREATE TABLE AudienceReviews (
+    idAudienceReview INT AUTO_INCREMENT NOT NULL UNIQUE,
+    idMovie INT NOT NULL,
+    idAudience INT NOT NULL,
+    review VARCHAR(300) NOT NULL,
+    stars TINYINT NOT NULL CHECK (stars BETWEEN 1 AND 5),
+    PRIMARY KEY (idAudienceReview),
+    FOREIGN KEY (idMovie) REFERENCES Movies (idMovie) ON DELETE CASCADE,
+    FOREIGN KEY (idAudience) REFERENCES Audiences (idAudience) ON DELETE CASCADE,
+    CONSTRAINT movie_audience UNIQUE (idMovie, idAudience)
 );
 
-INSERT INTO AudienceReviews(
-  review,
-  idMovie,
-  idAudience,
-  stars
-)
-VALUES(
-  "This movie is life-changing!",
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idAudience FROM Audiences WHERE firstName = "John" AND lastName = "Adams"),
-  5
-),
-(
-  "Mid",
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idAudience FROM Audiences WHERE firstName = "Adam" AND lastName = "James"),
-  3
-),
-(
-  "Hate the cliff-hanger, for that 1 star.",
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idAudience FROM Audiences WHERE firstName = "Jawan" AND lastName = "Curry"),
-  1
-);
+INSERT INTO
+    AudienceReviews (
+        review,
+        idMovie,
+        idAudience,
+        stars
+    )
+VALUES (
+        "This movie is life-changing!",
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idAudience
+            FROM Audiences
+            WHERE
+                firstName = "John"
+                AND lastName = "Adams"
+        ),
+        5
+    ),
+    (
+        "Mid",
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idAudience
+            FROM Audiences
+            WHERE
+                firstName = "Adam"
+                AND lastName = "James"
+        ),
+        3
+    ),
+    (
+        "Hate the cliff-hanger, for that 1 star.",
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idAudience
+            FROM Audiences
+            WHERE
+                firstName = "Jawan"
+                AND lastName = "Curry"
+        ),
+        1
+    );
 
 -- -----------------------------------------------------
 -- Movies_has_Actors Intersection Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Movies_has_Actors`;
-CREATE TABLE Movies_has_Actors(
-  idMovies_has_Actors INT AUTO_INCREMENT NOT NULL UNIQUE,
-  idMovie INT NOT NULL,
-  idActor INT NOT NULL,
-  PRIMARY KEY (idMovies_has_Actors),
-  FOREIGN KEY (idMovie) REFERENCES Movies(idMovie)
-    ON DELETE CASCADE,
-  FOREIGN KEY (idActor) REFERENCES Actors(idActor)
-    ON DELETE CASCADE,
-  CONSTRAINT movie_actor UNIQUE(idMovie, idActor)
+
+CREATE TABLE Movies_has_Actors (
+    idMovies_has_Actors INT AUTO_INCREMENT NOT NULL UNIQUE,
+    idMovie INT NOT NULL,
+    idActor INT NOT NULL,
+    PRIMARY KEY (idMovies_has_Actors),
+    FOREIGN KEY (idMovie) REFERENCES Movies (idMovie) ON DELETE CASCADE,
+    FOREIGN KEY (idActor) REFERENCES Actors (idActor) ON DELETE CASCADE,
+    CONSTRAINT movie_actor UNIQUE (idMovie, idActor)
 );
 
-INSERT INTO Movies_has_Actors(
-   idMovie,
-   idActor
-)
-VALUES
-(
-   (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-   (SELECT idActor FROM Actors WHERE firstName = "Shameik" AND lastName = "Moore")
-),
-(
-   (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-   (SELECT idActor FROM Actors WHERE firstName = "Hailee" AND lastName = "Steinfeld")
-),
-(
-   (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-   (SELECT idActor FROM Actors WHERE firstName = "Brian" AND lastName = "Henry" AND middleName = "Tyree")
-);
+INSERT INTO
+    Movies_has_Actors (idMovie, idActor)
+VALUES (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idActor
+            FROM Actors
+            WHERE
+                firstName = "Shameik"
+                AND lastName = "Moore"
+        )
+    ),
+    (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idActor
+            FROM Actors
+            WHERE
+                firstName = "Hailee"
+                AND lastName = "Steinfeld"
+        )
+    ),
+    (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idActor
+            FROM Actors
+            WHERE
+                firstName = "Brian"
+                AND lastName = "Henry"
+                AND middleName = "Tyree"
+        )
+    );
 
 -- -----------------------------------------------------
 -- Movies_has_Directors Intersection Table & Insert Values
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Movies_has_Directors`;
-CREATE TABLE Movies_has_Directors(
-  idMovies_has_Directors INT AUTO_INCREMENT NOT NULL UNIQUE,
-  idMovie INT NOT NULL,
-  idDirector INT NOT NULL,
-  PRIMARY KEY (idMovies_has_Directors),
-  FOREIGN KEY (idMovie) REFERENCES Movies(idMovie)
-    ON DELETE CASCADE,
-  FOREIGN KEY (idDirector) REFERENCES Directors(idDirector)
-    ON DELETE CASCADE,
-  CONSTRAINT movie_directors UNIQUE(idMovie, idDirector)
+
+CREATE TABLE Movies_has_Directors (
+    idMovies_has_Directors INT AUTO_INCREMENT NOT NULL UNIQUE,
+    idMovie INT NOT NULL,
+    idDirector INT NOT NULL,
+    PRIMARY KEY (idMovies_has_Directors),
+    FOREIGN KEY (idMovie) REFERENCES Movies (idMovie) ON DELETE CASCADE,
+    FOREIGN KEY (idDirector) REFERENCES Directors (idDirector) ON DELETE CASCADE,
+    CONSTRAINT movie_directors UNIQUE (idMovie, idDirector)
 );
 
-INSERT INTO Movies_has_Directors(
-  idMovie,
-  idDirector
-)
-VALUES(
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idDirector FROM Directors WHERE firstName = "Joaquim" AND lastName = "Dos Santos")
-),
-(
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idDirector FROM Directors WHERE firstName = "Kemp" AND lastName = "Powers")
-),
-(
-  (SELECT idMovie FROM Movies WHERE title = "Spider-Man: Across the Spider Verse"),
-  (SELECT idDirector FROM Directors WHERE firstName = "Justin" AND lastName = "Thompson")
-);
+INSERT INTO
+    Movies_has_Directors (idMovie, idDirector)
+VALUES (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idDirector
+            FROM Directors
+            WHERE
+                firstName = "Joaquim"
+                AND lastName = "Dos Santos"
+        )
+    ),
+    (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idDirector
+            FROM Directors
+            WHERE
+                firstName = "Kemp"
+                AND lastName = "Powers"
+        )
+    ),
+    (
+        (
+            SELECT idMovie
+            FROM Movies
+            WHERE
+                title = "Spider-Man: Across the Spider Verse"
+        ),
+        (
+            SELECT idDirector
+            FROM Directors
+            WHERE
+                firstName = "Justin"
+                AND lastName = "Thompson"
+        )
+    );
 
-SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS = 1;
+
 COMMIT;
